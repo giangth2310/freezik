3096e9107f8d0b4151d70108cb6e4f3cc32a3c49
DGiang <macosx@Giangs-MacBook-Pro.local>
2018-11-14 20:15:13 +0700

    api top authors

--- backend/models/author.js
+++ backend/models/author.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const AuthorSchema = mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
   name: {
     type: String,
     required: true
@@ -19,8 +22,31 @@ const AuthorSchema = mongoose.Schema({
   }
 });
 
-AuthorSchema.statics.getAll = () => {
-  return Author.find().select('-password -__v');
+AuthorSchema.statics.getTopAuthors = () => {
+  return Author.aggregate([
+    {
+      $lookup: {
+        from: 'musics',
+        localField: '_id',
+        foreignField: 'authorId',
+        as: 'musics'
+      }
+    }, {
+      $unwind: "$musics"
+    }, {
+      $group: {
+        _id: "$_id",
+        name: { $first: "$name" },
+        avatar: { $first: "$avatar" },
+        email: { $first: "$email" },
+        totalViews: { $sum: '$musics.views' }
+      }
+    }, {
+      $sort: { totalViews: -1 }
+    }, {
+      $limit: 5
+    }
+  ]);
 }
 const Author = mongoose.model('authors', AuthorSchema);
 
